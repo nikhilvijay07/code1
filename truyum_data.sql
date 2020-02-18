42fb951fe9c92fa8c0fee259e9774286638997bf
nikhilvijay07 <61176202+nikhilvijay07@users.noreply.github.com>
2020-02-18 10:28:24 +0530

    Add files via upload

---
+++ truyum_data.sql
@@ -0,0 +1,10 @@
+use truyum;
+CREATE TABLE menu_item(Name VARCHAR(30),Price VARCHAR(30),Active VARCHAR(5),Date_of_Launch DATE,Category varchar(30),Free_Delivery VARCHAR(5));
+INSERT INTO menu_item VALUES
+("Sandwich","Rs 99.00","Yes",'2017-03-15',"Main Course","Yes"),
+("Burger","Rs 129.00","Yes",'2017-12-',"Main Course","Yes"),
+("Pizza","Rs 149.00","Yes",),
+(),
+(),
+()
+
